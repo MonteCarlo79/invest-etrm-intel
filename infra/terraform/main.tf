3d10e93ee247ae48bd2f55fc0a08354394fa3767
Dipeng Chen <dipeng.chen@envision-energy.com>
2026-03-28 14:26:06 +0800

    Increase inner pipeline task memory to 8GB

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -989,8 +989,8 @@ resource "aws_ecs_task_definition" "inner_pipeline" {
   family                   = "${var.name}-inner-pipeline"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 1024
-  memory                   = 2048
+  cpu                      = 2048
+  memory                   = 8192
 
   execution_role_arn = aws_iam_role.task_execution.arn
   task_role_arn      = aws_iam_role.task_role.arn
